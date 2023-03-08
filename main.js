656e954a849841df109706bab8c748d3e778e711
Kamron <koraboevkamron@gmail.com>
2023-03-08 15:09:35 +0500

    save

---
+++ main.js
@@ -0,0 +1,56 @@
+var time = +prompt('Введите время в цифрах');
+switch(time){
+    case 1:
+    alert('Час ночи'); break;
+    case 2:
+    alert('Два часа ночи'); break;
+    case 3:
+    alert('Три часа ночи'); break;
+    case 4:
+    alert('Четыри часа ночи'); break;
+    case 5:
+    alert('5 часов'); break;
+    case 6:
+    alert ('6 часов'); break;
+    case 7:
+    alert ('7 часов'); break;
+    case 8:
+    alert('8 часов'); break;
+    case 9:
+    alert ('9 часов'); break;
+    case 10:
+    alert ('10 часов'); break;
+    case 11:
+    alert('11 часов '); break;
+    case 12:
+    alert('12 часов'); break;
+    case 13:
+    alert('Час дня'); break;
+    case 14:
+    alert('2 часа дня'); break;
+    case 15:
+    alert('3 часа дня'); break;
+    case 16:
+    alert('4 часа дня'); break;
+    case 17:
+    alert ('5 часа вечера '); break;
+    case 18:
+    alert('6 часа вечера'); break;
+    case 19:
+    alert('7 часа вечера'); break;
+    case 20:
+    alert('8 часа вечера'); break;
+    case 21:
+    alert('9 часа вечера'); break;
+    case 22:
+    alert('10 часа вечера'); break;
+    case 23:
+    alert('11 часа вечера'); break;
+    case 00:
+    alert('12 часов ночи');
+    break;
+    default:
+    alert('Error');
+
+
+}
